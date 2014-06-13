98cf27905db8287c942752fd0acf2adc83509f23
TakahiroTsujiura <b1012178@fun.ac.jp>
2014-06-13 14:21:14 +0900

    ViewControllerを2つ追加

---
+++ ShipApp/ShipApp/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  ShipApp
+//
+//  Created by codepro on 2014/06/13.
+//  Copyright (c) 2014年 FutureUniversityHakodate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
---
+++ ShipApp/ShipApp/ShipSummaryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShipSummaryTableViewController.h
+//  ShipApp
+//
+//  Created by codepro on 2014/06/13.
+//  Copyright (c) 2014年 FutureUniversityHakodate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShipSummaryTableViewController : UITableViewController
+
+@end
