55675950926f227915c34afb95cbfc1f8a36b79a
TakahiroTsujiura <b1012178@fun.ac.jp>
2014-07-14 17:12:16 +0900

    画面を縦に対応

---
+++ ShipApp/ShipApp/otherViewController.h
@@ -0,0 +1,14 @@
+//
+//  otherViewController.h
+//  ShipApp
+//
+//  Created by codepro on 2014/06/30.
+//  Copyright (c) 2014年 FutureUniversityHakodate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface otherViewController : UIViewController
+
+@end
